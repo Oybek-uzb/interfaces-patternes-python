672f99b3c61d58bb7b61cc81377df96d8e549c8a
Oybek <programmingiswonderful@gmail.com>
2022-02-10 20:24:19 +0500

    cop-client

--- patterns/cop_client.py
+++ patterns/cop_client.py
@@ -2,29 +2,44 @@ from patterns.chain_of_responsibility import AbstractLoadHandler
 
 
 class HardwareLoadHandler(AbstractLoadHandler):
-    def handle(self, request: dict) -> bool:
-        stat = request.get("stat")
+    def handle(self, req: dict) -> bool:
+        stat = req.get("stat")
         if stat is None:
             stat = {}
-        cpu = request.get("cpu")
+        cpu = req.get("cpu")
         stat["cpu"] = cpu != ""
-        request["stat"] = stat
+        req["stat"] = stat
         if cpu != "":
-            return super().handle(request=request)
+            return super().handle(request=req)
         else:
             return False
 
 
 class BIOSLoadHandler(AbstractLoadHandler):
-    def handle(self, request: dict) -> bool:
-        stat = request.get("stat")
+    def handle(self, req: dict) -> bool:
+        stat = req.get("stat")
         if stat is None:
             stat = {}
-        bios = request.get("bios")
+        bios = req.get("bios")
         stat["bios"] = bios != ""
-        request["stat"] = stat
+        req["stat"] = stat
         if bios != "":
-            return super().handle(request=request)
+            return super().handle(request=req)
+        else:
+            return False
+
+
+class RAMLoadHandler(AbstractLoadHandler):
+    def handle(self, req: dict) -> bool:
+        print("RAM")
+        stat = req.get("stat")
+        if stat is None:
+            stat = {}
+        ram = req.get("ram")
+        stat["ram"] = ram != ""
+        req["stat"] = stat
+        if ram != "":
+            return super().handle(request=req)
         else:
             return False
 
@@ -32,13 +47,16 @@ class BIOSLoadHandler(AbstractLoadHandler):
 if __name__ == '__main__':
     hw_handler = HardwareLoadHandler()
     bios_handler = BIOSLoadHandler()
+    ram_handler = RAMLoadHandler()
 
     hw_handler.set_next(bios_handler)
+    bios_handler.set_next(ram_handler)
 
     request = {
-        "cpu": "AMD",
-        "bios": "American Award BIOS"
+        "cpu": "ASDAA",
+        "bios": "",
+        "ram": "True"
     }
 
-    print(hw_handler.handle(request=request))
+    print(hw_handler.handle(req=request))
     print(request)
