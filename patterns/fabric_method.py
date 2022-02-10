bf73dfe9618f27a49afe0a23aaf2d1deea44a5be
Oybek <programmingiswonderful@gmail.com>
2022-02-10 20:44:42 +0500

    fabric-method

---
+++ patterns/fabric_method.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+
+
+class Cellphone(ABC):
+    @abstractmethod
+    def call(self) -> str:
+        pass
+
+
+class CellphoneCompany(ABC):
+    @abstractmethod
+    def make_phone(self):
+        pass
+
+    def do_call(self) -> str:
+        product = self.make_phone()
+
+        # work with product
+        result = product.call()
+
+        return result
+
+
+class Apple(CellphoneCompany):
+    def make_phone(self) -> Cellphone:
+        return IPhone()
+
+
+class Google(CellphoneCompany):
+    def make_phone(self) -> Cellphone:
+        return Pixel()
+
+
+class IPhone(Cellphone):
+    def call(self) -> str:
+        return "Call from IPhone"
+
+
+class Pixel(Cellphone):
+    def call(self) -> str:
+        return "Call from Pixel"
+
+
+if __name__ == '__main__':
+    print(Apple().do_call())
+    print(Google().do_call())
